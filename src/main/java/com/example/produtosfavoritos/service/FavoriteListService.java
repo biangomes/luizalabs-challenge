8c8f3264dc5ac158fe1c8a79e23789e4e3666513
biangomes <beanascigom@gmail.com>
2023-03-23 10:06:07 -0500

    Methods to work with FavoriteList

--- src/main/java/com/example/produtosfavoritos/service/FavoriteListService.java
+++ src/main/java/com/example/produtosfavoritos/service/FavoriteListService.java
@@ -1,5 +1,7 @@
 package com.example.produtosfavoritos.service;
 
+import com.example.produtosfavoritos.model.Client;
+import com.example.produtosfavoritos.model.FavoritesProducts;
 import com.example.produtosfavoritos.model.Product;
 import com.example.produtosfavoritos.repository.FavoriteListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +15,20 @@ public class FavoriteListService {
     @Autowired
     private FavoriteListRepository repo;
 
-    public List<Product> getFavoriteListForAClient(Long id) {
-        List<Product> records = repo.findAllFavoritesProducts(id);
+    public List<Product> getFavoriteListForAClient(Long client_id) {
+        List<Product> records = repo.findAllByClient(client_id);
         return records;
+    }
+
+    public List<Client> getAllClientWhichFavoriteProductIs(Long product_id) {
+        List<Client> records = repo.findAllByProductId(product_id);
+        return records;
+    }
+
+    public FavoritesProducts save(Client client, List<Product> products) {
+        FavoritesProducts favoritesProducts = new FavoritesProducts();
+
+        favoritesProducts.setClient(client);
+
     }
 }
