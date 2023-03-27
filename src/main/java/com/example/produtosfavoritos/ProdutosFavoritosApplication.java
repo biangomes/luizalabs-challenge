02446cc86ad9b14f619ac5409030941f8a96268d
biangomes <biangomes0707@gmail.com>
2023-03-27 16:25:32 -0300

    Retirei código comentado e iniciei o teste unitário de Update de Client

--- src/main/java/com/example/produtosfavoritos/ProdutosFavoritosApplication.java
+++ src/main/java/com/example/produtosfavoritos/ProdutosFavoritosApplication.java
@@ -2,11 +2,7 @@ package com.example.produtosfavoritos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
 public class ProdutosFavoritosApplication {
--- src/test/java/com/example/produtosfavoritos/service/ClientServiceTest.java
+++ src/test/java/com/example/produtosfavoritos/service/ClientServiceTest.java
@@ -1,8 +1,6 @@
 package com.example.produtosfavoritos.service;
 
 import com.example.produtosfavoritos.model.Client;
-import com.example.produtosfavoritos.model.FavoritesProducts;
-import com.example.produtosfavoritos.model.Product;
 import com.example.produtosfavoritos.repository.ClientRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,16 +9,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -46,7 +38,6 @@ public class ClientServiceTest {
 
         assertThat(actualClient).usingRecursiveComparison().isEqualTo(clientToSave);
         verify(clientRepository, times(1)).save(any(Client.class));
-        //verifyNoMoreInteractions(clientRepository);
     }
 
     @Test
@@ -61,25 +52,6 @@ public class ClientServiceTest {
 
         Assertions.assertTrue(result);
     }
-
-//    @Test
-//    void mustGetAllClients() {
-//        // Preparing data
-//        Mockito.when(clientRepository.findAll()).thenReturn(Arrays.asList(
-//                new Client(1L,
-//                        "Alex Raider",
-//                        "alexra1der@hotmail.com", (List<FavoritesProducts>) new FavoritesProducts()),
-//
-//                new Client(2L,
-//                        "Angelina Jolie",
-//                        "jolie@outlook.com",
-//                        (List<FavoritesProducts>) new FavoritesProducts())));
-
-
-        // Testing
-//        List<Client> clients = clientRepository.findAll();
-//        Assertions.assertEquals(clients.get(0), );
-//    }
 
     @Test
     void mustGetAClientById() {
@@ -96,7 +68,15 @@ public class ClientServiceTest {
         verify(clientRepository, times(1)).findById(any(Client.class).getClientId());
     }
 
-//    void clientMustHaveASingleListOfFavoritesProducts() {
-//
-//    }
+    @Test
+    void mustUpdateAClient() {
+        Client client = Client.builder()
+            .clientId(1L)
+            .name("Peter Parker")
+            .email("peter.parker@gmail.com")
+            .password("@123")
+            .build();
+
+        //when(clientRepository)
+    }
 }
